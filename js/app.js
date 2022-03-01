397fee543e28c03f8d7d3b953ca55ee10a4ee95c
mdmuzahid07 <mdmuzahid7396@gmail.com>
2022-03-01 16:15:00 +0600

    update code

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const loadData = () => {
     
     // load data by API
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then(Response => Response.json())
         .then(data => displayData(data.data))
@@ -30,15 +30,27 @@ const displayData = (phones) => {
         div.classList.add('div');
         div.innerHTML = ` 
             <div class="col">
-                <div class="card p-3 rounded-3 border-0 h-100">
+                <div class="card p-3 rounded-3 border-0 h-75">
                     <img src="${phone.image}" class="card-img-top" alt="phone image">
                     <div class="card-body">
                         <h5 class="card-title">Brand: ${phone.brand}</h5>
-                        <p class="card-text">Model: ${phone.phone_name}</p>
+                        <p class="card-text">Model: ${phone.phone_name}<i onclick="phoneDetailsById(${phone.slug})" class="fa-solid fa-angles-right ms-1 d-inline boxshadow p-1 rounded-3"></i></p>
                     </div>
+                    
                 </div>
             </div> 
         `;
         dataContainer.appendChild(div);
     });
-}
+};
+
+
+
+// display phone full details
+
+// const phoneDetailsById = (id) => {
+//     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }

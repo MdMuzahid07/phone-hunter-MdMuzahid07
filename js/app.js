87eab2b3c57dd11bdfeceb61ba24b0bcd9e023f8
mdmuzahid07 <mdmuzahid7396@gmail.com>
2022-03-02 01:42:09 +0600

    almost complete

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const loadData = () => {
     // get input value
 
+    // loading
+    document.getElementById('loading').style.display = "block";
+
     const searchFiled = document.getElementById('search-filed');
-    const searchValue = searchFiled.value;
+    const searchValue = searchFiled.value.toLowerCase();
 
     searchFiled.value = " ";
 
@@ -20,28 +23,37 @@ const loadData = () => {
 // display data on website
 
 const displayData = (phones) => {
+    // console.log(phones)
     const dataContainer = document.getElementById('search-results');
     // remove search result on every search
     dataContainer.textContent = " ";
 
-    phones.forEach((phone) => {
-        console.log(phone)
-        const div = document.createElement("div");
-        // div.classList.add('div');
-        div.innerHTML = ` 
-            <div class="col">
-                <div class="card p-3 rounded-3 border-0 h-100 w-100 card-shadow">
-                    <img src="${phone.image}" class="card-img-top" alt="phone image">
-                    <div class="card-body">
-                        <h5 class="card-title">Brand: ${phone.brand}</h5>
-                        <p class="card-text">Model: ${phone.phone_name}<i onclick="phoneDetailsById('${phone.slug}')" class="fa-solid fa-angles-right ms-1 d-inline boxshadow p-1 rounded-3"></i></p>
+    //loading 
+    document.getElementById('loading').style.display = "none";
+
+
+    if (phones.length === 0) {
+        document.getElementById('result-error').style.display = "block";
+    }
+    else {
+        phones.forEach((phone) => {
+            console.log(phone)
+            const div = document.createElement("div");
+            div.innerHTML = ` 
+                <div class="col">
+                    <div class="card p-3 rounded-3 border-0 h-100 w-100 card-shadow">
+                        <img src="${phone.image}" class="card-img-top" alt="phone image">
+                        <div class="card-body">
+                            <h5 class="card-title">Brand: ${phone.brand}</h5>
+                            <p class="card-text">Model: ${phone.phone_name}<i onclick="phoneDetailsById('${phone.slug}')" class="fa-solid fa-angles-right ms-1 d-inline boxshadow p-1 rounded-3"></i></p>
+                        </div>
+                        
                     </div>
-                    
-                </div>
-            </div> 
-        `;
-        dataContainer.appendChild(div);
-    });
+                </div> 
+            `;
+            dataContainer.appendChild(div);
+        });
+    }
 };
 
 
@@ -83,10 +95,28 @@ const phoneFullDetails = (phoneFeatures) => {
                     <h3>sensors:</h3>
                     <p class="card-text">sensors: ${phoneFeatures.mainFeatures.sensors}</p>
                     <p class="card-text">storage: ${phoneFeatures.mainFeatures.storage}</p>
-
                 </div>
             </div>
-        </div> 
-    `;
+        </div>
+        `;
     phoneDetails.appendChild(div);
+    
+    // phone others features
+   
+    const otherFeatures = document.createElement('div');
+    otherFeatures.innerHTML = `
+    <div class="card-shadow p-3">
+                        <h1>Others features:</h1>
+                        <p class="card-text">Bluetooth: ${phoneFeatures.others.Bluetooth}</p>
+                        <p class="card-text">GPS: ${phoneFeatures.others.GPS}</p>
+                        <p class="card-text">NFC: ${phoneFeatures.others.NFC}</p>
+                        <p class="card-text">Radio: ${phoneFeatures.others.Radio}</p>
+                        <p class="card-text">USB: ${phoneFeatures.others.USB}</p>
+                        <p class="card-text">WLAN: ${phoneFeatures.others.WLAN}</p>
+    </div>
+    
+    `;
+
+    phoneDetails.appendChild(otherFeatures);
+                      
 };
